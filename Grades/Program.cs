7d9c64356bf76a562ed0307cbaddc6f6cd6ae725
RickieCanCodeIT <rickie@WeCanCodeIT.org>
2021-03-17 15:31:38 -0400

    Initial commit

---
+++ Grades/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<int> grades = new List<int>();
+            names.Add("Sam");
+            grades.Add(70);
+            names.Add("Chris");
+            grades.Add(95);
+            names.Add("Casey");
+            grades.Add(85);
+            names.Add("Rufio");
+            grades.Add(30);
+            Console.WriteLine("Student grades:");
+            double sum = 0.0;
+
+            for (int counter = 0; counter < grades.Count; counter++)
+            {
+                Console.WriteLine(names[counter] + " received a grade of " + grades[counter]);
+                sum = grades[counter];
+            }
+            double average = sum / grades.Count;
+            Console.WriteLine("Average grade: " + average);
+        }
+    }
+}
